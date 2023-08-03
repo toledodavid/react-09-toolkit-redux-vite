a37d396caa2aa311f221cec18253104a0856fa88
David Toledo <dtoledoz0207@gmail.com>
2023-08-02 23:16:33 -0600

    Create PokemonApp component and create pokemonSlice and use it in store file

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,16 @@
+
+
+export const PokemonApp = () => {
+  return(
+    <>
+      <h1>Pokemon App</h1>
+      <hr />
+
+      <ul>
+        <li>Hola</li>
+        <li>Hola</li>
+        <li>Hola</li>
+      </ul>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import { store } from './store';
 
 import App from './App.jsx'
 import './index.css'
+import { PokemonApp } from './PokemonApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>,
 )
