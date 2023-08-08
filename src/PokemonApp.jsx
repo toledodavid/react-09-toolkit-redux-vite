ff86369b9fa94816fed54cdf64818d5b29695c0e
David Toledo <dtoledoz0207@gmail.com>
2023-08-08 13:25:21 -0600

    Select pokemons from store and show them in PokemonApp component

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon';
 
 
 export const PokemonApp = () => {
 
   const dispatch = useDispatch();
+  const { pokemons, isLoading, page } = useSelector(state => state.pokemons);
 
   useEffect(() => {
     dispatch(getPokemons());
@@ -16,11 +17,17 @@ export const PokemonApp = () => {
       <h1>Pokemon App</h1>
       <hr />
 
+      <span>Loading: {isLoading ? 'True' : 'False'}</span>
+
       <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
+        {
+          pokemons.map(pokemon => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))
+        }
       </ul>
+
+      <button disabled={isLoading} onClick={ () => dispatch(getPokemons(page))}>Next</button>
     </>
   );
 }
