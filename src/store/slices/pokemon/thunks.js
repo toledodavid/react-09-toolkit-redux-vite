10a3adf5263433422ff52711c389ffc09a4ddcb0
David Toledo <dtoledoz0207@gmail.com>
2023-08-08 11:44:10 -0600

    Create first function thunks to get pokemons

---
+++ src/store/slices/pokemon/thunks.js
@@ -0,0 +1,9 @@
+import { setPokemons, startLoadingPokemons } from './pokemonSlice';
+
+export const getPokemons = (page = 0) => {
+  return async (dispatch, getState) => {
+    dispatch(startLoadingPokemons());
+
+    // dispatch(setPokemons());
+  }
+}
