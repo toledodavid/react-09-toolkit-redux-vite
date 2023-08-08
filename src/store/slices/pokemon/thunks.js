8c5222717f68956f80845714d0ae56df470f1e6a
David Toledo <dtoledoz0207@gmail.com>
2023-08-08 12:09:52 -0600

    Install axios to make a request to pokeapi and save the pokemons in state

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,9 +1,17 @@
+import { pokemonApi } from '../../../api/pokemonApi';
 import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
-    // dispatch(setPokemons());
+    // const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+    // const data = await response.json();
+    // console.log(data);
+
+    const {data} = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
+    console.log(data);
+
+    dispatch(setPokemons({page: page + 1, pokemons: data.results}));
   }
 }
