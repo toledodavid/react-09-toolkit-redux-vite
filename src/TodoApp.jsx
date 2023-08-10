7034a305f485411f399d45a7a7ac2eef736b40f9
David Toledo <dtoledoz0207@gmail.com>
2023-08-10 11:17:40 -0600

    Implement our custom hook useGetTodosQuery to get todos from api and show them in TodoApp component

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,13 +1,27 @@
+import { useGetTodosQuery } from './store/apis';
+
+
 
 export const TodoApp = () => {
+
+  const {data: todos = [], isLoading} = useGetTodosQuery();
+
   return(
     <>
       <h1>Todos - RTK Query</h1>
       <hr />
 
-      <h4>isLoading...</h4>
+      <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
 
       <pre></pre>
+
+      <ul>
+        {
+          todos.map(todo => (
+            <li key={todo.id}>{todo.title}</li>
+          ))
+        }
+      </ul>
 
       <button>Next todo</button>
     </>
