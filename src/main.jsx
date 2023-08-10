94370dffb96cda75b132dae02772db103f8e86c0
David Toledo <dtoledoz0207@gmail.com>
2023-08-10 10:48:52 -0600

    Create TodoApp Component and todosApi file to implement RTK Query functionality from @reduxjs/toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,14 @@ import { store } from './store';
 import App from './App.jsx'
 import './index.css'
 import { PokemonApp } from './PokemonApp';
+import { TodoApp } from './TodoApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      <TodoApp />
     </Provider>
   </React.StrictMode>,
 )
